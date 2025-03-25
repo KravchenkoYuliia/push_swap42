20c5bbac9182aac8089f0fd6c6ee2d1d3747e509
Yuliia Kravchenko <yukravch@paul-f3Ar5s4.clusters.42paris.fr>
2025-03-25 18:06:45 +0100

    added  function to put error message, free every allocated list and exit

---
+++ sources/exit.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   exit.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yukravch <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/25 14:58:12 by yukravch          #+#    #+#             */
+/*   Updated: 2025/03/25 18:05:00 by yukravch         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	ft_exit(char *msg, t_list **a, t_list **b)
+{
+	// & a -> l'adresse de a => jamais nulle
+	// a -> le contenu de a => l'adresse de la variable a dans main()
+	// *a -> la valeur contenu dans la variable a dans main()
+
+	if (a && *a) {
+		ft_lstclear(a, &free);
+		ft_printf("clear a\n");
+	}
+        if (b && *b)
+		ft_lstclear(b, &free);
+	ft_printf("Error\n%s\n", msg);
+	exit(EXIT_FAILURE);
+}
