683a354ec1c2924d955747bf30f4c8032949ed26
Yuliia Kravchenko <yukravch@paul-f3Ar5s4.clusters.42paris.fr>
2025-03-25 18:08:06 +0100

    Errors handling: some arguments not being integers, some arguments exceeding the integer limits, and/or the presence of duplicates.

--- sources/main.c
+++ sources/main.c
@@ -6,12 +6,12 @@
 /*   By: yukravch <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/24 11:24:43 by yukravch          #+#    #+#             */
-/*   Updated: 2025/03/24 18:16:54 by yukravch         ###   ########.fr       */
+/*   Updated: 2025/03/25 17:58:51 by yukravch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <stdio.h>
 #include "push_swap.h"
-
 void	ft_show(t_list* list)
 {
 	while (list)
@@ -19,10 +19,21 @@ void	ft_show(t_list* list)
 		ft_printf("%d ", *((int *)(list->content)));
 		list = list->next;
 	}
+//	write(1, "\n", 1);
 }
 
+int	ft_is_overflow(t_list *list)
+{
+	while (list)
+	{
+		if (*(long *)list->content < INT_MIN || *(long *)list->content > INT_MAX)
+			return (1);
+		list = list->next;
+	}
+	return (0);
+}
 
-int	ft_is_duplicate(t_list* list)
+int	ft_is_duplicate(t_list *list)
 {
 	t_list	*temp;
 
@@ -40,9 +51,9 @@ int	ft_is_duplicate(t_list* list)
 	return (0);
 }
 
-int	*ft_malloc_number(number)
+long	*ft_malloc_number(long number)
 {
-	int	*nb = (int *)malloc(sizeof(int));
+	long	*nb = (long *)malloc(sizeof(long));
 	if (!nb)
 		return (NULL);
 	*nb = number;
@@ -54,7 +65,7 @@ int	ft_put_to_stack_a(char *str, t_list **a)
 	int	i;
 	t_list	*new;
 	char	**numbers;
-	int	*nb;
+	long	*nb;
 
 	i = 0;
 	numbers = ft_split(str, ' ');
@@ -62,9 +73,36 @@ int	ft_put_to_stack_a(char *str, t_list **a)
 		return (1);
 	while (numbers[i])
 	{
-		nb = ft_malloc_number(atoi(numbers[i]));
+		nb = ft_malloc_number(ft_atol(numbers[i]));
 		new = ft_lstnew(nb);
 		ft_lstadd_back(a, new);
+		free(numbers[i]);
+		i++;
+	}
+	free(numbers);
+	return (0);
+}
+
+int	ft_wrong_argv_format(int ac, char **av)
+{
+	int	i;
+	int	j;
+	char	letter;
+
+	i = 0;
+	j = 0;
+	while (av[i] && i < ac)
+	{
+		j = 0;
+		while (av[i][j])
+		{
+			letter = av[i][j];
+			if (!ft_isdigit(letter) && letter != '-' && letter != '+' && letter != ' ')
+				return (1);
+			if ((letter == '-' || letter == '+') && !ft_isdigit(av[i][j + 1]))
+				return (1);
+			j++;
+		}
 		i++;
 	}
 	return (0);
@@ -76,6 +114,12 @@ int	main(int ac, char **av)
 	t_list	*a = NULL;
 	t_list	*b = NULL;
 
+	ft_printf("a address:%p\n", &a);
+	ft_exit("Exemple", &a, &b);
+	if (ac == 1)
+		ft_exit("Put at least one argument", NULL, NULL);
+	if (ft_wrong_argv_format(ac - 1, av + 1))
+		ft_exit("Put only positive/negative integer or space", NULL, NULL);
 	i = 1;
 	while (i < ac)
 	{
@@ -83,8 +127,12 @@ int	main(int ac, char **av)
 			return (0);
 		i++;
 	}
+	if (ft_is_overflow(a))
+		ft_exit("Put number between INT_MIN and INT_MAX", &a, &b);
 	if (ft_is_duplicate(a))
-		ft_printf("Error\nThere are duplicate numbers\n");
+		ft_exit("There are duplicate numbers", &a, &b);
 	ft_show(a);
 	ft_show(b);
+	ft_lstclear(&a, &free);
+	ft_lstclear(&b, &free);
 }
