c943010cda379063a024dd915174dc3dd2a1ed51
Yuliia Kravchenko <yukravch@bess-f2r3s12.clusters.42paris.fr>
2025-03-27 13:31:57 +0100

    Changed  variable name to a more suitable word

--- library/libft/sources/ft_lstnew.c
+++ library/libft/sources/ft_lstnew.c
@@ -6,19 +6,19 @@
 /*   By: yukravch <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/22 15:10:14 by yukravch          #+#    #+#             */
-/*   Updated: 2024/11/24 12:07:39 by yukravch         ###   ########.fr       */
+/*   Updated: 2025/03/27 13:08:49 by yukravch         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "libft.h"
 
 t_list	*ft_lstnew(void *content)
 {
-	t_list	*array;
+	t_list	*lst;
 
-	array = (t_list *)malloc(sizeof(t_list));
-	if (!array)
+	lst = (t_list *)malloc(sizeof(t_list));
+	if (!lst)
 		return (NULL);
-	array->content = content;
-	array->next = NULL;
-	return (array);
+	lst->content = content;
+	lst->next = NULL;
+	return (lst);
 }
